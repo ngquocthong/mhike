446fcf4435652e33c860932b3a4fb7823bdbe579
firoz3130 <firosk7@gmail.com>
2023-07-20 11:56:46 +0530

    Changed the gallery option to camera uploads

--- lib/capture.dart
+++ lib/capture.dart
@@ -52,7 +52,7 @@ class _CaptureState extends State<Capture> {
                   // Open camera or gallery
                   ImagePicker imagePicker = ImagePicker();
                   XFile? file =
-                      await imagePicker.pickImage(source: ImageSource.gallery);
+                      await imagePicker.pickImage(source: ImageSource.camera);
                   print('${file?.path}');
                   if (file == null) return;
 
