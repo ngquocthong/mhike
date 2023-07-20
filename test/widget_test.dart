c70b3590901030dc726d09557bcf322bd56f6f5c
firoz3130 <firosk7@gmail.com>
2023-07-20 18:26:17 +0530

    Widget test file modified

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,28 +1,35 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-// import 'package:flutter/material.dart';
-//import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart'; // Import the Description widget
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  // Build our app and trigger a frame.
-  //await tester.pumpWidget(const MyApp());
+  // Define a test case for the Description widget
+  testWidgets('Test Description Widget', (WidgetTester tester) async {
+    // Build the Description widget and trigger a frame.
+    // await tester.pumpWidget(
+    //   MaterialApp(
+    //     home: Description(
+    //       imageUrl: 'https://firebasestorage.googleapis.com/v0/b/tripiton-fe9c9.appspot.com/o/images%2F1688375048528?alt=media&token=9c8ee7d8-4f61-49b3-ac7a-da274303784c', // Replace with a valid image URL
+    //       placeName: 'Venice', // Replace with a place name for testing
+    //       description: ' Venice is a city in northeastern Italy, in the Veneto region. It is situated on a group of 118 small islands that are separated by canals and linked by bridges. The historic center of Venice is divided into six districts, or sestieri, which are named Cannaregio, Castello, Dorsoduro, San Marco, San Polo, and Santa Croce.  Venice is known as the "City of Canals" and is one of the most popular tourist destinations in the world. The city is famous for its beautiful bridges, gondola rides, atmospheric streets, and carnival celebrations.  Venice has a long and rich history. The city was founded in the 5th century AD by refugees from the mainland who were fleeing from barbarian invasions. Venice became a major trading power during the Middle Ages and Renaissance, and it was a staging area for the Crusades. The citys decline began in the 18th century, and it was finally annexed by Italy in 1866.  Today, Venice is a popular tourist destination, but it is also facing a number of challenges. The city is sinking at an alarming rate, and it is also struggling to cope with the large number of tourists who visit each year.', // Replace with a description for testing
+    //     ),
+    //   ),
+    // );
 
-  // Verify that our counter starts at 0.
-  // expect(find.text('0'), findsOneWidget);
-  // expect(find.text('1'), findsNothing);
+    // Verify that the widgets you expect are displayed using 'expect' statements.
+    // For example, you can find the text widget with the description and check if it is present.
+    expect(find.text('Description text'), findsOneWidget);
 
-  // // Tap the '+' icon and trigger a frame.
-  // await tester.tap(find.byIcon(Icons.add));
-  // await tester.pump();
+    // You can also find other widgets, like Image widgets, and check if they are displayed.
+    expect(find.byType(Image), findsOneWidget);
 
-  // // Verify that our counter has incremented.
-  // expect(find.text('0'), findsNothing);
-  // expect(find.text('1'), findsOneWidget);
-  // });
+    // Test widget interactions if applicable. For example, tap on buttons or icons.
+    // Here, we can find the home icon button and tap it.
+    await tester.tap(find.byIcon(Icons.home));
+    await tester.pump();
+
+    // Verify the result of the interaction.
+    // For example, you can check if the navigation to the home page was successful.
+    expect(find.text('Home Page'),
+        findsOneWidget); // Replace 'Home Page' with a text that should appear on the home page.
+  });
 }
