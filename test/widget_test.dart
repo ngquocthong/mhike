35aaa78f8d9b920b90778a026c857a6cf90016b6
firoz3130 <firosk7@gmail.com>
2023-07-20 19:10:05 +0530

    New unit tests are added for Group page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,35 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  testWidgets('Test Description Widget', (WidgetTester tester) async {
-    // Build the Description widget and trigger a frame.
-    // await tester.pumpWidget(
-    //   MaterialApp(
-    //     home: Description(
-    //       imageUrl: 'your_image_url',
-    //       placeName: 'Place Name',
-    //       description: 'Description text',
-    //     ),
-    //   ),
-    // );
+// void main() {
+//   testWidgets('Test Description Widget', (WidgetTester tester) async {
+//     // Build the Description widget and trigger a frame.
+//     // await tester.pumpWidget(
+//     //   MaterialApp(
+//     //     home: Description(
+//     //       imageUrl: 'your_image_url',
+//     //       placeName: 'Place Name',
+//     //       description: 'Description text',
+//     //     ),
+//     //   ),
+//     // );
 
-    // Verify that the widgets you expect are displayed using 'expect' statements.
-    // Find the widgets using 'find.text' and 'find.byType' as appropriate.
+//     // Verify that the widgets you expect are displayed using 'expect' statements.
+//     // Find the widgets using 'find.text' and 'find.byType' as appropriate.
 
-    // Example: Verify that the text 'Description text' is present.
-    // expect(find.text('Description text'), findsOneWidget);
+//     // Example: Verify that the text 'Description text' is present.
+//     // expect(find.text('Description text'), findsOneWidget);
 
-    // Example: Verify that an Image widget is displayed.
-    //expect(find.byType(Image), findsOneWidget);
+//     // Example: Verify that an Image widget is displayed.
+//     //expect(find.byType(Image), findsOneWidget);
 
-    // Test widget interactions if applicable. For example, tap on buttons or icons.
-    // Here, we can find the home icon button and tap it.
-    await tester.tap(find.byIcon(Icons.home));
-    await tester.pumpAndSettle(); // Wait for animations to complete.
+//     // Test widget interactions if applicable. For example, tap on buttons or icons.
+//     // Here, we can find the home icon button and tap it.
+//     await tester.tap(find.byIcon(Icons.home));
+//     await tester.pumpAndSettle(); // Wait for animations to complete.
 
-    // Verify the result of the interaction.
-    // For example, you can check if the navigation to the home page was successful.
-    expect(find.text('Home Page'), findsOneWidget);
-  });
-}
+//     // Verify the result of the interaction.
+//     // For example, you can check if the navigation to the home page was successful.
+//     expect(find.text('Home Page'), findsOneWidget);
+//   });
+// }
